d40c1ac11bb764092702f44e76b6be088274bb44
zuya <kazu.prgrm555y@gmail.com>
2025-12-26 22:27:46 +0900

    add: 🎸 add components parts

---
+++ app/components/Button.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+type Props = {
+  onClick?: () => void;
+  children: ReactNode;
+};
+
+function Button({ onClick, children }: Props) {
+  return (
+    <>
+      <button
+        className="w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
+
+export default Button;
--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,50 @@
+"use client";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Button from "./Button";
+
 function Nav() {
-  return <div>nav parts</div>;
+  const { isAuthenticated } = useKindeBrowserClient();
+  const isAuth = isAuthenticated;
+
+  const selectedCamper = () => {
+    console.log("selectedCamper");
+  };
+  const selectedShop = () => {
+    console.log("selectedShop");
+  };
+
+  return (
+    <>
+      <div className="w-full h-auto md:w-1/2 md:h-screen bg-[#1A1A1A] border-r border-[#2A2A2A]">
+        <div>
+          {/* ユーザ切り替え */}
+          <div className="border border-b border-[#2A2A2A] p-4 gap-4 flex">
+            <Button onClick={selectedCamper}>キャンパー</Button>
+            <Button onClick={selectedShop}>店舗</Button>
+          </div>
+
+          {isAuth ? (
+            /* 店舗 */
+            <div className="flex md:flex-col p-4 md:gap-y-4">
+              <button>ダッシュボード</button>
+              <button>要望一覧</button>
+              <button>利用者分布</button>
+              <button>
+                <LogoutLink>ログアウト</LogoutLink>
+              </button>
+            </div>
+          ) : (
+            /* キャンパー */
+            <div className="flex md:flex-col p-4 md:gap-y-4">
+              <button>ダッシュボード</button>
+              <button>要望送信</button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Nav;
