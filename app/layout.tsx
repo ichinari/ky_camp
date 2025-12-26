8b4a3c5dd6d8421c48051c4c6a378163b8f2b666
zuya <kazu.prgrm555y@gmail.com>
2025-12-26 22:28:52 +0900

    refactor: 💡 fix page and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,27 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata, Viewport } from "next";
 
-export default function RootLayout({
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+};
+
+export const metadata: Metadata = {
+  title: "KY Camp",
+  description: "Camping platform",
+};
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body
         suppressHydrationWarning
-        className="min-h-screen text-white bg-[#111111]"
+        className="h-screen text-white bg-[#111111]"
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,25 @@
 "use client";
-import { LoginLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
 import Link from "next/link";
 
 function MemberTop() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="w-5/12 border-2 border-[#2A2A2A] rounded-lg bg-[#1A1A1A] flex  items-center justify-evenly gap-y-3.5 p-3.5">
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-evenly w-11/12 sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 gap-y-3.5 p-3.5 bg-[#1A1A1A] border-2 border-[#2A2A2A] rounded-lg">
         <div className="text-center">
           <div className="mb-3">キャンパーの方はこちら</div>
-          <div className="bg-[#4FA3A5] text-[#EDEDED] p-3 hover:bg-[#5FB385] rounded-lg w-full cursor-pointer">
-            <Link href="/dashboard/member">ダッシュボード</Link>
-          </div>
+          <Link
+            href="/dashboard/member"
+            className="block w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]"
+          >
+            ダッシュボード
+          </Link>
         </div>
         <div className="text-center">
           <div className="mb-3">店舗の方はこちら</div>
-          <div className="bg-[#4FA3A5] text-[#EDEDED] p-3 hover:bg-[#5FB385] rounded-lg w-full cursor-pointer">
-            <LoginLink>ログイン</LoginLink>
-          </div>
+          <LoginLink className="block w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]">
+            ログイン
+          </LoginLink>
         </div>
       </div>
     </div>
