5b38065b506e7d9838c7c3d94ae1a88295058e13
zuya <kazu.prgrm555y@gmail.com>
2025-12-26 22:52:18 +0900

    refactor: 💡 add props type

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
 
 type Props = {
+  type?: "button" | "submit" | "reset";
   onClick?: () => void;
   children: ReactNode;
 };
 
-function Button({ onClick, children }: Props) {
+function Button({ type = "button", onClick, children }: Props) {
   return (
     <button
+      type={type}
       className="w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]"
       onClick={onClick}
     >
