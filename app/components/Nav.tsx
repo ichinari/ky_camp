6778e218322966a0ed3cd83b3f7713a30265b937
zuya <kazu.prgrm555y@gmail.com>
2025-12-27 08:15:13 +0900

    refactor: 💡 changed path and function name, delete unnecessary tag

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import Button from "./Button";
 
@@ -21,8 +21,8 @@ function Nav() {
   const selectedRequestList = () => {
     console.log("selectedRequestList");
   };
-  const selectedCampoerDistribution = () => {
-    console.log("selectedCampoerDistribution");
+  const selectedCamperDistribution = () => {
+    console.log("selectedCamperDistribution");
   };
 
   const selectedCamperDashBoard = () => {
@@ -33,35 +33,33 @@ function Nav() {
   };
 
   return (
-    <>
-      <div className="w-full h-auto md:w-1/2 md:h-screen bg-[#1A1A1A] border-r border-[#2A2A2A]">
-        <div>
-          {/* ユーザ切り替え */}
-          <div className="border border-b border-[#2A2A2A] p-4 gap-4 flex">
-            <Button onClick={selectedCamper}>キャンパー</Button>
-            <Button onClick={selectedShop}>店舗</Button>
-          </div>
-
-          {isAuthenticated ? (
-            /* 店舗 */
-            <div className="flex md:flex-col p-4 md:gap-y-4">
-              <Button onClick={selectedShopDashBoard}>ダッシュボード</Button>
-              <Button onClick={selectedRequestList}>要望一覧</Button>
-              <Button onClick={selectedCampoerDistribution}>利用者分布</Button>
-              <Button>
-                <LogoutLink>ログアウト</LogoutLink>
-              </Button>
-            </div>
-          ) : (
-            /* キャンパー */
-            <div className="flex md:flex-col p-4 md:gap-y-4">
-              <Button onClick={selectedCamperDashBoard}>ダッシュボード</Button>
-              <Button onClick={selectedCamperRequest}>要望送信</Button>
-            </div>
-          )}
+    <div className="w-full h-auto md:w-1/2 md:h-screen bg-[#1A1A1A] border-r border-[#2A2A2A]">
+      <div>
+        {/* ユーザ切り替え */}
+        <div className="border border-b border-[#2A2A2A] p-4 gap-4 flex">
+          <Button onClick={selectedCamper}>キャンパー</Button>
+          <Button onClick={selectedShop}>店舗</Button>
         </div>
+
+        {isAuthenticated ? (
+          /* 店舗 */
+          <div className="flex md:flex-col p-4 md:gap-y-4">
+            <Button onClick={selectedShopDashBoard}>ダッシュボード</Button>
+            <Button onClick={selectedRequestList}>要望一覧</Button>
+            <Button onClick={selectedCamperDistribution}>利用者分布</Button>
+            <Button>
+              <LogoutLink>ログアウト</LogoutLink>
+            </Button>
+          </div>
+        ) : (
+          /* キャンパー */
+          <div className="flex md:flex-col p-4 md:gap-y-4">
+            <Button onClick={selectedCamperDashBoard}>ダッシュボード</Button>
+            <Button onClick={selectedCamperRequest}>要望送信</Button>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
