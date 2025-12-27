8617719e5bbbe940fd27be462af8a4b5c658acf5
zuya <kazu.prgrm555y@gmail.com>
2025-12-28 00:13:34 +0900

    refactor: 💡 changed bg color setting

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,20 +2,29 @@ import { ReactNode } from "react";
 
 type Props = {
   type?: "button" | "submit" | "reset";
+  isActive?: Boolean;
   onClick?: () => void;
   children: ReactNode;
 };
 
-function Button({ type = "button", onClick, children }: Props) {
+function Button({
+  type = "button",
+  isActive = false,
+  onClick,
+  children,
+}: Props) {
   return (
     <button
       type={type}
-      className="w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]"
+      className={`w-full p-3 rounded-lg cursor-pointer flex items-center justify-center gap-2 ${
+        isActive
+          ? "bg-[#4FA3A5] text-[#EDEDED]"
+          : "bg-transparent text-[#9A9A9A] border border-[#2A2A2A]"
+      }`}
       onClick={onClick}
     >
       {children}
     </button>
   );
 }
-
 export default Button;
