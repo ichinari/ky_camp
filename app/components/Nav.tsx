ce414644cd7b4e2421aec6fcbcc4666a1ae20d0e
zuya <kazu.prgrm555y@gmail.com>
2025-12-28 00:14:08 +0900

    refactor: 💡 changed toggle function and add icon

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useState } from "react";
 import Button from "./Button";
+import Icons from "./Icons";
 
-function Nav() {
+type Props = {
+  type?: "camper" | "shop";
+};
+
+function Nav({ type = "camper" }: Props) {
   const { isAuthenticated } = useKindeBrowserClient();
+  const [isCampoer, setIsCamper] = useState<Boolean>(type === "camper");
 
   /* ユーザ切り替え*/
-  const selectedCamper = () => {
-    console.log("selectedCamper");
-  };
-  const selectedShop = () => {
-    console.log("selectedShop");
+  const toggleUser = (user: String) => {
+    const result = user === "camper";
+    setIsCamper(result);
   };
 
   /* ユーザコンテンツ切り替え */
@@ -37,40 +42,61 @@ function Nav() {
       <div>
         {/* ユーザ切り替え */}
         <div className="border border-b border-[#2A2A2A] p-4 gap-4 flex">
-          <Button onClick={selectedCamper}>キャンパー</Button>
-          <Button onClick={selectedShop}>店舗</Button>
+          <Button onClick={() => toggleUser("camper")} isActive={isCampoer}>
+            <Icons iconName="camper" />
+            キャンパー
+          </Button>
+          <Button onClick={() => toggleUser("shop")} isActive={!isCampoer}>
+            <Icons iconName="shop" />
+            店舗
+          </Button>
         </div>
 
-        {isAuthenticated ? (
+        {isCampoer ? (
           /* 店舗 */
           <ul className="flex md:flex-col p-4 md:gap-y-4">
             <li>
-              <Button onClick={selectedShopDashBoard}>ダッシュボード</Button>
+              <Button onClick={selectedShopDashBoard} isActive={false}>
+                <Icons iconName="dashboard" />
+                ダッシュボード
+              </Button>
             </li>
 
             <li>
-              <Button onClick={selectedRequestList}>要望一覧</Button>
+              <Button onClick={selectedRequestList} isActive={false}>
+                <Icons iconName="send" />
+                要望一覧
+              </Button>
             </li>
             <li>
-              <Button onClick={selectedCamperDistribution}>利用者分布</Button>
+              <Button onClick={selectedCamperDistribution} isActive={false}>
+                <Icons iconName="graph" />
+                利用者分布
+              </Button>
             </li>
-            <li>
-              <LogoutLink
-                className="w-full p-3 bg-[#4FA3A5] text-[#EDEDED] text-center rounded-lg
-            cursor-pointer hover:bg-[#5FB385]"
-              >
-                ログアウト
-              </LogoutLink>
-            </li>
+            {isAuthenticated && (
+              <li>
+                <LogoutLink className="flex items-center justify-center gap-2 w-full p-3 bg-[#4FA3A5] text-[#EDEDED] rounded-lg cursor-pointer hover:bg-[#5FB385]">
+                  <Icons iconName="logout" />
+                  ログアウト
+                </LogoutLink>
+              </li>
+            )}
           </ul>
         ) : (
           /* キャンパー */
           <ul className="flex md:flex-col p-4 md:gap-y-4">
             <li>
-              <Button onClick={selectedCamperDashBoard}>ダッシュボード</Button>
+              <Button onClick={selectedCamperDashBoard} isActive={false}>
+                <Icons iconName="dashboard" />
+                ダッシュボード
+              </Button>
             </li>
             <li>
-              <Button onClick={selectedCamperRequest}>要望送信</Button>
+              <Button onClick={selectedCamperRequest} isActive={false}>
+                <Icons iconName="send" />
+                要望送信
+              </Button>
             </li>
           </ul>
         )}
