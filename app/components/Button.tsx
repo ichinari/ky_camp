e90e63f0d70f29ab9292b607b2704d7cad893c52
zuya <kazu.prgrm555y@gmail.com>
2025-12-30 10:58:10 +0900

    refactor: 💡 add disabled function

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,7 +2,8 @@ import { ReactNode } from "react";
 
 type Props = {
   type?: "button" | "submit" | "reset";
-  isActive?: Boolean;
+  isActive?: boolean;
+  isDisabled?: boolean;
   onClick?: () => void;
   children: ReactNode;
 };
@@ -10,17 +11,21 @@ type Props = {
 function Button({
   type = "button",
   isActive = false,
+  isDisabled = false,
   onClick,
   children,
 }: Props) {
   return (
     <button
       type={type}
-      className={`w-full p-3 rounded-lg cursor-pointer flex items-center justify-center gap-2 ${
-        isActive
-          ? "bg-[#4FA3A5] text-[#EDEDED]"
-          : "bg-transparent text-[#9A9A9A] border border-[#2A2A2A]"
+      className={`w-full p-3 rounded-lg flex items-center justify-center gap-2 ${
+        isDisabled
+          ? "bg-[#2A2A2A] text-[#6A6A6A] cursor-not-allowed opacity-50"
+          : isActive
+          ? "bg-[#4FA3A5] text-[#EDEDED] cursor-pointer"
+          : "bg-transparent text-[#9A9A9A] border border-[#2A2A2A] cursor-pointer hover:bg-[#4FA3A5] hover:text-[#EDEDED] hover:border-[#4FA3A5]"
       }`}
+      disabled={isDisabled}
       onClick={onClick}
     >
       {children}
